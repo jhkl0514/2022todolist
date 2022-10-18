a002c2a244131f07866a2d57c9789ee963daeb73
jhkl0514 <jhkl0514@naver.com>
2022-10-18 11:17:41 +0900

    inti

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="searchInput">
+    <input type="text" class="inp" placeholder="오늘 할일"
+    v-model="newTodoItem">
+    <div class="btn" @click="todoItem">할일</div>
+</div>
+<!-- {{newTodoItem}} -->
+
+</template>
+
+<script>
+export default {
+   data(){
+    return{
+       newTodoItem:'' 
+    }
+   },
+   methods:{
+    todoItem(){
+        // console.log("todoItem");
+    this.$emit("addTodo",this.newTodoItem)    
+    }
+   }
+}
+
+</script>
+
+<style lang="scss">
+.searchInput{
+    display: flex;
+    .inp{
+        flex: 1 0 auto;
+        text-indent: 10px;
+    }
+    .btn{
+        line-height: 40px;
+        padding: 0 10px;
+        background: gray;
+        color: #fff;
+        cursor:pointer;
+    }
+}
+</style>
